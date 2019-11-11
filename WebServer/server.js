f6482619466088192a478df953f3dd561484b0b9
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-11 13:22:22 +0200

    Update server.js

--- WebServer/server.js
+++ WebServer/server.js
@@ -1,15 +1,27 @@
 const http = require('http');
+const url = require('url');
 const port = 8080;
 
 const app = http.createServer(function (req, res) {
-    res.write('Hello');
-    setTimeout(
-        function () {
-            res.write(' ');
-            res.end('World!')
-        },
-        5000
-    )
+    // res.write('Hello');
+    // setTimeout(
+    //     function () {
+    //         res.write(' ');
+    //         res.end('World!')
+    //     },
+    //     5000
+    // )
+
+    const { pathname } = url.parse(req['url']);
+    if(pathname === '/'){
+        res.end('Home!')
+        return
+    }
+    
+    if (pathname ==='/about') {
+        res.end('About')
+        return
+    }
 });
 
 app.listen(port, function () {
