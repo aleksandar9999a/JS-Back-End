abbd883b50527086d02c3a5c05c334ac5cb9737d
aleksandar9999a <aleksandar9999a@gmail.com>
2019-11-11 11:52:36 +0200

    Install lodash and simple demo for asynchronous programming

--- Node.js/demo.js
+++ Node.js/demo.js
@@ -1,1 +1,13 @@
-console.log('Hello World!')
+const fileManger = require('./file-manger');
+
+fileManger.readUsers(function(err, content){
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    console.log(content);
+});
+
+
+console.log('Hello World!');
