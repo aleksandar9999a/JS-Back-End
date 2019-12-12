5d986138fe325fbd2291c4059cc078222e2d6669
aleksandar9999a <aleksandar9999a@gmail.com>
2019-12-12 05:17:10 +0200

    demo

--- ExpressJS and Templating/index.js
+++ ExpressJS and Templating/index.js
@@ -2,8 +2,21 @@ const express = require('express');
 const port = 8080;
 const app = express();
 
+const users = [
+    {
+        id: 1,
+        name: 'Pesho'
+    }
+]
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!');
+})
+
+
+app.get('/user/:id', (req, res) => {
+    const user = users.find( x => x.id === Number(req.params.id));
+    res.send(user)
 })
 
 app.listen(port, () => {
