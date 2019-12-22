07bc7fc9009e6b3a38a83ca5db1512f09597fc45
aleksandar9999a <aleksandar9999a@gmail.com>
2019-12-22 12:03:37 +0200

    Added not found page

--- ExpressJS and Templating - Workshop/config/routes.js
+++ ExpressJS and Templating - Workshop/config/routes.js
@@ -1,10 +1,12 @@
 const cubeController = module.require('../controllers/cube')
 const aboutController = module.require('../controllers/about')
 const createController = module.require('../controllers/create')
+const notFound = module.require('../controllers/notFound')
 
 module.exports = (app) => {
     app.get('/', cubeController.index)
     app.get('/details/:id', cubeController.details)
     app.get('/create', createController)
     app.get('/about', aboutController)
+    app.get('/*', notFound)
 };
---
+++ ExpressJS and Templating - Workshop/controllers/notFound.js
@@ -0,0 +1,5 @@
+function notFound(req, res) {
+    res.render('404.hbs')
+}
+
+module.exports = notFound;
