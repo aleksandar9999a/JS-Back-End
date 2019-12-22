301631cf5ea32cee9107942cd8d0c581872a2b81
aleksandar9999a <aleksandar9999a@gmail.com>
2019-12-22 11:13:08 +0200

    Render first page

--- ExpressJS and Templating - Workshop/index.js
+++ ExpressJS and Templating - Workshop/index.js
@@ -1,17 +1,10 @@
 const env = process.env.NODE_ENV || 'development';
 global.__basedir = __dirname;
 
-const cubeModel = require('./models/cube');
+const config = require('./config/config')[env];
+const app = require('express')();
 
-cubeModel.insert({ name: 'test1', desc: 'test1'}).then(insertedCube => {
-    console.log(insertedCube);
-    return cubeModel.delete(insertedCube.id).then(() => console.log('deleted'));
-})
+require('./config/express')(app);
+require('./config/routes')(app);
 
-// const config = require('./config/config')[env];
-// const app = require('express')();
-
-// require('./config/express')(app);
-// require('./config/routes')(app);
-
-// app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
