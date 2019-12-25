0e2ff2986cd41571350c9fc7cfd97ef16e29c351
aleksandar9999a <aleksandar9999a@gmail.com>
2019-12-25 06:24:59 +0200

    Mongo demo

--- ExpressJS and Templating - Workshop/index.js
+++ ExpressJS and Templating - Workshop/index.js
@@ -1,10 +1,30 @@
-const env = process.env.NODE_ENV || 'development';
-global.__basedir = __dirname;
+const dbUrl = 'mongodb://localhost:27017';
+const { MongoClient } = require('mongodb');
+const client = new MongoClient(dbUrl);
+client.connect(function(err){
+    if (err) { console.error(err); return }
+    
+    const db = client.db('test');
+    const users = db.collection('users');
 
-const config = require('./config/config')[env];
-const app = require('express')();
+    users.deleteMany({name: 'Pavel'}).then(deleteEntity => {
+        console.log(deleteEntity);
+        
+    })
+    
+})
 
-require('./config/express')(app);
-require('./config/routes')(app);
 
-app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+
+
+
+// const env = process.env.NODE_ENV || 'development';
+// global.__basedir = __dirname;
+
+// const config = require('./config/config')[env];
+// const app = require('express')();
+
+// require('./config/express')(app);
+// require('./config/routes')(app);
+
+// app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
