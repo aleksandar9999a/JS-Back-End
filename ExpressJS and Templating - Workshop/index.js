11e6c76af72b539c139084de80bd909c855a4598
aleksandar9999a <aleksandar9999a@gmail.com>
2019-12-25 06:27:04 +0200

    Install mongoose and return old server

--- ExpressJS and Templating - Workshop/index.js
+++ ExpressJS and Templating - Workshop/index.js
@@ -1,30 +1,27 @@
-const dbUrl = 'mongodb://localhost:27017';
-const { MongoClient } = require('mongodb');
-const client = new MongoClient(dbUrl);
-client.connect(function(err){
-    if (err) { console.error(err); return }
+// const dbUrl = 'mongodb://localhost:27017';
+// const { MongoClient } = require('mongodb');
+// const client = new MongoClient(dbUrl);
+// client.connect(function(err){
+//     if (err) { console.error(err); return }
     
-    const db = client.db('test');
-    const users = db.collection('users');
+//     const db = client.db('test');
+//     const users = db.collection('users');
 
-    users.deleteMany({name: 'Pavel'}).then(deleteEntity => {
-        console.log(deleteEntity);
+//     users.deleteMany({name: 'Pavel'}).then(deleteEntity => {
+//         console.log(deleteEntity);
         
-    })
+//     })
     
-})
+// })
 
 
+const env = process.env.NODE_ENV || 'development';
+global.__basedir = __dirname;
 
+const config = require('./config/config')[env];
+const app = require('express')();
 
+require('./config/express')(app);
+require('./config/routes')(app);
 
-// const env = process.env.NODE_ENV || 'development';
-// global.__basedir = __dirname;
-
-// const config = require('./config/config')[env];
-// const app = require('express')();
-
-// require('./config/express')(app);
-// require('./config/routes')(app);
-
-// app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
