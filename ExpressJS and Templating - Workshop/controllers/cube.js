5e9d84a606b14e5f2877be128352f7031900ad59
aleksandar9999a <aleksandar9999a@gmail.com>
2019-12-25 07:01:59 +0200

    Added new cube model

--- ExpressJS and Templating - Workshop/controllers/cube.js
+++ ExpressJS and Templating - Workshop/controllers/cube.js
@@ -1,8 +1,25 @@
 const cubeModel = require('../models/cube')
 
 function index(req, res, next) {
-    cubeModel.getAll().then(cubes => {
-        res.render('index.hbs', { cubes })
+    // const { from, to, search } = req.query;
+    // const findFn = item => {
+    //     let result = true;
+    //     if (search) {
+    //         result = item.name.toLowerCase().includes(search);
+    //     }
+    //     if (result && from) {
+    //         result = Number(item.difficultyLevel) >= Number(from);
+    //     }
+    //     if (result && to) {
+    //         result = Number(item.difficultyLevel) <= Number(to);
+    //     }
+    //     return result;
+    // }
+    
+    cubeModel.find().then(cubes => {
+        res.render('index.hbs', { cubes, 
+            // search, from, to 
+        })
     }).catch(next)
 }
 
---
+++ ExpressJS and Templating - Workshop/models/_cube.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const path = require('path');
+
+class CubeModel {
+    constructor() {
+        this.data = require('../config/database');
+    }
+
+    _write(newData, resolveData) {
+        return new Promise((res, rej) => {
+            fs.writeFile(path.resolve('config/database.json'), JSON.stringify(newData, null, 2), (err) => {
+                if (err) { rej(err); return; }
+
+                this.data = newData;
+                res(resolveData)
+            })
+        })
+    }
+
+    create(name, description, imageUrl, defficultyLevel) {
+        return { name, description, imageUrl, defficultyLevel }
+    }
+
+    insert(newCube) {
+        const newIndex = this.data.lastIndex + 1;
+        newCube = { id: newIndex, ...newCube };
+        const newData = {
+            lastIndex: newIndex,
+            entities: this.data.entities.concat(newCube)
+        };
+
+        return this._write(newData, newCube);
+    }
+
+    update(currId, updates) {
+        const entityIndex = this.data.entities.findIndex(({ id }) => id === currId);
+        const entity = this.data.entities[entityIndex];
+        const updatedEntity = { ...entity, ...updates };
+        const newData = {
+            lastIndex: this.data.lastIndex,
+            entities: [...this.data.entities.slice(0, entityIndex), updatedEntity, ...this.data.entities.slice(entityIndex + 1)]
+        }
+
+        return this._write(newData, updatedEntity);
+    }
+
+    delete(id) {
+        const deletedEntity = this.getOne(id);
+
+        const newData = {
+            lastIndex: this.data.lastIndex,
+            entities: this.data.entities.filter(({ id: i }) => i !== id)
+        };
+
+
+        return this._write(newData, deletedEntity);
+    }
+
+    getOne(id) {
+        return Promise.resolve(this.data.entities.find(({ id: i }) => i == id));
+    }
+
+    getAll() {
+        return Promise.resolve(this.data.entities);
+    }
+}
+
+module.exports = new CubeModel();
