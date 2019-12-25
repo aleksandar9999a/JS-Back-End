f2a6954c16fc3e9830e528a331c349e271c1202f
aleksandar9999a <aleksandar9999a@gmail.com>
2019-12-25 06:42:42 +0200

    Added url to config and mongoose connector

--- ExpressJS and Templating - Workshop/index.js
+++ ExpressJS and Templating - Workshop/index.js
@@ -1,27 +1,26 @@
+global.__basedir = __dirname;
+const dbConnector = require('./config/db');
+
+dbConnector().then(() => {
+    const config = require('./config/config');
+    const app = require('express')();
+    require('./config/express')(app);
+    require('./config/routes')(app);
+
+    app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+}).catch(console.error)
+
+
 // const dbUrl = 'mongodb://localhost:27017';
 // const { MongoClient } = require('mongodb');
 // const client = new MongoClient(dbUrl);
 // client.connect(function(err){
 //     if (err) { console.error(err); return }
-    
+
 //     const db = client.db('test');
 //     const users = db.collection('users');
 
 //     users.deleteMany({name: 'Pavel'}).then(deleteEntity => {
 //         console.log(deleteEntity);
-        
 //     })
-    
 // })
-
-
-const env = process.env.NODE_ENV || 'development';
-global.__basedir = __dirname;
-
-const config = require('./config/config')[env];
-const app = require('express')();
-
-require('./config/express')(app);
-require('./config/routes')(app);
-
-app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
