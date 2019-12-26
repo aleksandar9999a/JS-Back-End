a6a7d90a25b0f5e17881a83b3866702406ef3014
aleksandar9999a <aleksandar9999a@gmail.com>
2019-12-26 10:04:24 +0200

    Added validations

--- ExpressJS and Templating - Workshop/models/cube.js
+++ ExpressJS and Templating - Workshop/models/cube.js
@@ -1,10 +1,26 @@
 const mongoose = require('mongoose');
 
 const cubeSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    imageUrl: String,
-    defficultyLevel: Number
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        max: 50,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        validate: {
+            validator: function (v) {
+                return /http/.test(v);
+            },
+            message: props => `${props.value} is not a valid link!`
+        },
+        required: true
+    },
+    difficultyLevel: Number
 });
 
 cubeSchema.methods.getDescription = function () {
