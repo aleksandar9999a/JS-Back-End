632163bae62557f55e899a641c9e089dcf113999
aleksandar9999a <aleksandar9999a@gmail.com>
2019-12-26 09:54:54 +0200

    Added new resources

--- ExpressJS and Templating - Workshop/controllers/create.js
+++ ExpressJS and Templating - Workshop/controllers/create.js
@@ -6,8 +6,6 @@ function loadCreateForm(req, res) {
 
 function createCube(req, res) {
     const { name = null, description = null, imageUrl = null, difficultyLevel = null } = req.body;
-    console.log(difficultyLevel);
-    
     cubeModel.create({ name, description, imageUrl, difficultyLevel }).then(insertedCube => res.redirect('/'));
 }
 
