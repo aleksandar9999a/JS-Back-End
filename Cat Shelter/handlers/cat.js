1df72792eca205facfe70c9c6de9f2850797ae88
Alexander Todorow <aleksandar9999a@gmail.com>
2020-04-13 15:00:06 +0300

    Refactoring

--- Cat Shelter/handlers/cat.js
+++ Cat Shelter/handlers/cat.js
@@ -6,53 +6,37 @@ const qs = require('querystring');
 const breeds = require('./../data/breeds');
 const cats = require('./../data/cats');
 
+function loadPath(pathUrl, res) {
+    const filePath = path.normalize(path.join(__dirname, pathUrl));
+        fs.readFile(filePath, function (err, data) {
+            if (err) {
+                console.log(err);
+                res.writeHead(404, {
+                    'Content-Type': 'text/plain'
+                });
+                res.write('Error in Home!');
+                res.end();
+                return;
+            }
+
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            res.write(data);
+            res.end();
+        });
+}
 
 module.exports = function (req, res) {
     const { pathname } = url.parse(req.url);
 
     if (pathname === '/cats/add-cat' && req.method === 'GET') {
-        const filePath = path.normalize(path.join(__dirname, './../views/addCat.html'));
-        fs.readFile(filePath, function (err, data) {
-            if (err) {
-                console.log(err);
-                res.writeHead(404, {
-                    'Content-Type': 'text/plain'
-                });
-                res.write('Error in Home!');
-                res.end();
-                return;
-            }
-
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            res.write(data);
-            res.end();
-        });
-
+        loadPath('./../views/addCat.html', res);
         return;
     }
 
     if (pathname === '/cats/add-breed' && req.method === 'GET') {
-        const filePath = path.normalize(path.join(__dirname, './../views/addBreed.html'));
-        fs.readFile(filePath, function (err, data) {
-            if (err) {
-                console.log(err);
-                res.writeHead(404, {
-                    'Content-Type': 'text/plain'
-                });
-                res.write('Error in Home!');
-                res.end();
-                return;
-            }
-
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            res.write(data);
-            res.end();
-        });
-
+        loadPath('./../views/addBreed.html', res);
         return;
     }
 
