e6d1c70a977d92c4ca5aec58c8ecc6c2b0d97972
Alexander Todorow <aleksandar9999a@gmail.com>
2020-04-14 14:26:08 +0300

    Extended

--- Cat Shelter/handlers/static-files.js
+++ Cat Shelter/handlers/static-files.js
@@ -1,53 +1,73 @@
 const url = require('url');
 const fs = require('fs');
 
+const types = {
+    'css': 'text/css',
+    'js': 'application/javascript',
+    'png': 'image/png',
+    'html': 'text/html',
+    'ico': 'image/png',
+    'jpg': 'image/png'
+}
+
 function getContentType(url) {
-    if (url.endsWith('css')) {
-        return 'text/css';
-    }
-
-    if (url.endsWith('html')) {
-        return 'text/html';
-    }
+    const key = Object.keys(types).filter(k => url.endsWith(k));
     
-    if (url.endsWith('png')) {
-        return 'text/png';
-    }
-
-    if (url.endsWith('js')) {
-        return 'text/js';
-    }
-    
-    if (url.endsWith('jpg')) {
-        return 'text/jpg';
+    if (key[0]) {
+        return types[key[0]];
     }
 
     return 'text/plain';
 }
 
-module.exports = function(req, res) {
+module.exports = function (req, res) {
     const { pathname } = url.parse(req.url);
 
     if (pathname.startsWith('/content') && req.method === 'GET') {
-        fs.readFile(`./${pathname}`, 'utf-8' ,function (err, data) {
-            if (err) {
-                console.log(err);
-                res.writeHead(404, {
-                    'Content-Type': 'text/plain'
+        if (pathname.endsWith('png') || pathname.endsWith('jpeg') || pathname.endsWith('jpg') && req.method === 'GET') {
+            fs.readFile(`./${pathname}`, function(err, data) {
+                if (err) {
+                    console.log(err);
+
+                    res.writeHead(404, {
+                        'Content-Type': 'text/plain'
+                    });
+
+                    res.write('Error was found');
+                    res.end();
+                    return;
+                }
+
+                res.writeHead(200, {
+                    'Content-Type': getContentType(pathname)
                 });
-                res.write('Error in Home!');
+
+                res.write(data);
                 res.end();
-                return;
-            }
-
-            res.writeHead(200, {
-                'Content-Type': getContentType(pathname)
             });
-            res.write(data);
-            res.end();
-        });
-        return;
-    }
+        } else {
+            fs.readFile(`.${pathname}`, 'utf-8', function(err, data) {
+                if (err) {
+                    console.log(err);
 
-    return true;
-}
+                    res.writeHead(404, {
+                        'Content-Type': 'text/plain'
+                    });
+
+                    res.write('Error was found');
+                    res.end();
+                    return;
+                }
+
+                res.writeHead(200, {
+                    'Content-Type': getContentType(pathname)
+                });
+
+                res.write(data);
+                res.end();
+            });
+        }
+    } else {
+        return true;
+    }
+};
