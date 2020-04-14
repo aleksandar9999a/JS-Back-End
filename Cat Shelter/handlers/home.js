b7821fd3e466f9bbb3c0bfbd97d781a13ebf3259
Alexander Todorow <aleksandar9999a@gmail.com>
2020-04-14 14:26:30 +0300

    Load cats from json

--- Cat Shelter/handlers/home.js
+++ Cat Shelter/handlers/home.js
@@ -1,28 +1,39 @@
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
-// const cats = require('./../data/cats');
+const cats = require('./../data/cats');
 
 module.exports = function (req, res) {
     const { pathname } = url.parse(req.url);
 
     if (pathname === '/' && req.method === 'GET') {
         const filePath = path.normalize(path.join(__dirname, './../views/home/index.html'));
-        fs.readFile(filePath, function (err, data) {
-            if (err) {
-                console.log(err);
-                res.writeHead(404, {
-                    'Content-Type': 'text/plain'
-                });
-                res.write('Error in Home!');
-                res.end();
-                return;
-            }
+        const index = fs.createReadStream(filePath);
 
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            res.write(data);
+        index.on('data', function (data) {
+
+            let modifiedCats = cats.map(cat => {
+                const img = path.join(`./content/images/${cat.image}`);
+                
+                return `<li>
+                    <img src="${img}" alt="${cat.name}">
+                    <h3>${cat.name}</h3>
+                    <p><span>Breed: </span>${cat.breed}</p>
+                    <p><span>Description: </span>${cat.description}</p>
+                    <ul class="buttons">
+						<li class="btn edit"><a href="/cats-edit/${cat.id}">Change Info</a></li>
+						<li class="btn delete"><a href="/cats-find-new-home/${cat.id}">New Home</a></li>
+					</ul>
+                </li>
+            `})
+
+            let modifiedData = data.toString().replace('{{cats}}', modifiedCats);
+            res.write(modifiedData);
+        })
+
+        index.on('error', console.log);
+
+        index.on('end', function () {
             res.end();
         });
 
